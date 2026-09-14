2bcefb0285cb41b3982037a323bde9363c81da16
TiliSpace <thilinia@gmail.com>
2026-09-14 20:25:42 +0000

    Updated main code with pandas

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-import streamlit as st
+import streamlit as st 
+import pandas
 
-st.title("Tils Demo Streamlit App")
-st.subheader('Streamlit integration testing for Pythin Automation')
-st.write(''' This demo is interesting...
+
+data = {
+  'Series_1':[1, 3, 4, 5, 7],
+  'Series 2':[10, 30, 40, 100, 250]
+}
+
+df = pandas.DataFrame(data)
+
+st.title('Our First Streamlit App')
+st.subheader('Introducing Streamlit in Automate Everything with Python')
+st.write('''This is our first Web App.
 Enjoy it!
 ''')
+st.write(df)
+st.line_chart(df)
+st.area_chart(df)
+
+myslider = st.slider('Celsius')
+st.write(myslider, 'in Fahrenheit is', myslider * 9/5 + 32)
