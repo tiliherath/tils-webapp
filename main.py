95e86067d857e112744139c933d5fd1fedc7baaf
TiliSpace <thilinia@gmail.com>
2026-09-14 20:23:52 +0000

    Create main code

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Tils Demo Streamlit App")
+st.subheader('Streamlit integration testing for Pythin Automation')
+st.write(''' This demo is interesting...
+Enjoy it!
+''')
